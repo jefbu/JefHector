b88fe7cbee64e1487f1346910039b0baaf05db58
jefbu <Jefbussens@gmail.com>
2017-02-02 21:07:31 +0100

    Jef - Mais um teste com system.out

--- Jef Heitor/src/global/GameManager.java
+++ Jef Heitor/src/global/GameManager.java
@@ -56,6 +56,7 @@ public abstract class GameManager {
 
 public static void main (String[] args){
 	System.out.println("wat");
+	System.out.println("Trump é um babaca");
 	gameModeId = 1;
 	gameStart = new GameStart ();
 	
