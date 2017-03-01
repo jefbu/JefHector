04fae75a887decf62ff4c417b074edffebcac69d
jefbu <Jefbussens@gmail.com>
2017-03-01 19:30:40 +0100

    teste 2

--- Jef Heitor/src/global/GameManager.java
+++ Jef Heitor/src/global/GameManager.java
@@ -17,7 +17,7 @@ public abstract class GameManager {
 	
 	static void gameIterate() {
 		
-		if (gameModeId == 1) {
+		if (gameModeId == 2) {
 			
 			bufferStrate = gameStart.getBufferStrategy();
 			
